5ba8657bf0f8c3570fcd95f455c4200af075a433
Pol Piedra <polpica@hotmail.com>
2019-05-02 10:37:37 +0200

    Review Functionality added

--- app/src/main/java/com/example/movielopp/activities/ReviewActivity.kt
+++ app/src/main/java/com/example/movielopp/activities/ReviewActivity.kt
@@ -1,32 +1,23 @@
 package com.example.movielopp.activities
 
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import com.example.movielopp.model.Movie
 import kotlinx.android.synthetic.main.activity_review.*
 import com.example.movielopp.R
+import com.example.movielopp.model.UserMovieRating
+import com.example.movielopp.model.UserMovieReview
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 
-class ReviewActivity : AppCompatActivity() /*, View.OnClickListener*/{
+class ReviewActivity : AppCompatActivity() {
 
-    /*override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.submitReviewTextView -> {
-                if (!reviewText.text.isEmpty() && !reviewConditionsOk) {
-                    Log.d("Test", "Button Clicked")
-                    Toast.makeText(applicationContext,
-                        "La crítica no a de estar vacía y tiene que tener un mínimo de 5 líneas.",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            }
-
-        }
-    }*/
 
     private var movieToWork: Movie? = null
 
@@ -38,30 +29,50 @@ class ReviewActivity : AppCompatActivity() /*, View.OnClickListener*/{
 
         movieToWork = intent.getParcelableExtra("movie")
 
-        checkMandatoryReviewConditions()
-         submitReviewTextView.setOnClickListener {
-            if (!reviewText.text.isEmpty() && !reviewConditionsOk) {
-                Log.d("Test", "Button Clicked")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        submitButtonActivity.setOnClickListener {
+            checkMandatoryReviewConditions()
+            if (reviewTextActivity.text.isEmpty() || !reviewConditionsOk) {
                 Toast.makeText(applicationContext,
                     "La crítica no a de estar vacía y tiene que tener un mínimo de 5 líneas.",
                     Toast.LENGTH_LONG
                 ).show()
+            } else {
+                insertReview()
             }
         }
     }
 
+    private fun insertReview() {
+        val auth = FirebaseAuth.getInstance()
+        val uidReview = UUID.randomUUID().toString()
+        val ref = FirebaseDatabase.getInstance().getReference("/ReviewMovie/$uidReview")
+
+        val userRating = UserMovieReview(uidReview, auth.currentUser!!.uid, movieToWork!!.id.toString(), reviewTextActivity.text.toString(), movieToWork!!.title!!)
+        ref.setValue(userRating)
+
+        val intent = Intent(this, MainActivity::class.java)
+        Log.d("FireBase", "Review inserted")
+        startActivity(intent)
+        finish()
+    }
+
     override fun onBackPressed() {
         super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
         finish()
     }
 
 
     private fun checkMandatoryReviewConditions() {
-        reviewText.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                reviewConditionsOk = reviewText.text.toString().trim().length < 5
-            }
+        if (reviewTextActivity.lineCount < 5) {
+            reviewConditionsOk = false
         }
+
     }
 }
 
--- app/src/main/java/com/example/movielopp/model/UserMovieReview.kt
+++ app/src/main/java/com/example/movielopp/model/UserMovieReview.kt
@@ -1,3 +1,3 @@
 package com.example.movielopp.model
 
-class UserMovieReview (val uidReview:String = "", val authorReview:String = "", val userUID:String = "", val movieID: String = "", var review:String = "" , var posterPath:String? = null)
+class UserMovieReview (val uidReview:String = "", val userUID:String = "", val movieID: String = "", var review:String = "" , var posterPath:String? = null)
